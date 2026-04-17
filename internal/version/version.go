56d012e7cd3f03b0bc1bc8c6c71e3e95ab10028b
Iván Salazar <ivangio.salazar@gmail.com>
2026-04-17 03:09:39 -0600

    chore: bump version to 0.1.1

---
+++ internal/version/version.go
@@ -0,0 +1,13 @@
+// Package version holds build-time version information.
+// Overridden at build time via ldflags:
+//
+//	go build -ldflags "-X github.com/birdple/falco/internal/version.Version=1.2.3 \
+//	                   -X github.com/birdple/falco/internal/version.Commit=abc1234"
+package version
+
+var (
+	// Version is the semantic version of the service.
+	Version = "0.1.1"
+	// Commit is the short git SHA baked in at build time.
+	Commit = "dev"
+)
